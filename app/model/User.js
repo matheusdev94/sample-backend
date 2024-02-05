7dd37c1265025c303849edd437601a322f167d6d
Matheus <matheus.dev94@gmail.com>
2024-02-04 22:24:49 -0300

    add user and employee model

---
+++ app/model/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const UserSchema = schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    User: {
+      type: Number,
+      default: 2001,
+    },
+    Editor: Number,
+    Admin: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: String,
+});
+
+module.exports = mongoose.model("User", UserSchema);
