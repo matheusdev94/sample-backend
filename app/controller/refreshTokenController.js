fcf22835f3e7edfcb74a77a0289397d4735104da
Matheus <matheus.dev94@gmail.com>
2024-03-12 22:00:44 -0300

    change access token expiration

--- app/controller/refreshTokenController.js
+++ app/controller/refreshTokenController.js
@@ -26,7 +26,7 @@ const handleRefreshToken = async (req, res) => {
               },
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: "60s" }
+            { expiresIn: "2m" }
           );
 
           return res.status(200).json({
